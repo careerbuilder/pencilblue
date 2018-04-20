a05958de698cefdd2eb2fee20e22ce578d327f56
Logan Genet <logangenet@yahoo.com>
2018-04-19 22:58:05 -0400

    Cleanup.  Admin index replacement is ready.

--- plugins/kronos/controllers/admin_index.js
+++ plugins/kronos/controllers/admin_index.js
@@ -4,31 +4,6 @@ module.exports = function IndexModule(pb) {
 
     class AdminIndex extends pb.BaseController {
 
-        async _getDummyServerClusterData () {
-            return pb.ServerRegistration.getInstance()
-                .getClusterStatusAsync();
-            // return [{
-            //     ip: '127.0.0.1',
-            //     port: '8080',
-            //     process_type: 'worker',
-            //     mem_usage: {
-            //         heapUsed: 100000
-            //     }
-            // }];
-        }
-        _getDummyAdminNav () {
-            return [
-                {text: 'Content', icon: 'fa-quote-right', link: '/admin/content'},
-                {text: 'Plugins', icon: 'fa-puzzle-piece', link: '/admin/plugins', children: [
-                        {text: "plugins", icon: 'fa-upload', link: '/admin/plugins'},
-                        {text: "themes", icon: 'fa-magic', link: '/admin/theme'}
-                    ]},
-                {text: 'Users', icon: 'fa-users', link: '/admin/users'},
-                {text: 'Settings', icon: 'fa-cogs', link: '/admin/settings'},
-                {text: 'View Site', icon: 'fa-desktop', link: '/'},
-                {text: 'Logout', icon: 'fa-power-off', link: '/actions/logout'}
-            ]
-        }
         async render (cb) {
             this.vueModelService = new (pb.PluginService.getService('VueModelRegistrationService', 'kronos', this.site))({ts: this.ts});
 
